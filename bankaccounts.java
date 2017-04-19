3fd43f9a7b46c06f44a922c52902c450a1cfed50
mikeman2020 <1996michaelt@gmail.com>
2017-04-20 00:58:25 +1200

    system print test
    attempting to print message to console - NOT WORKING

--- bankaccounts.java
+++ bankaccounts.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+
 public class bankaccounts{
-  public static void main(String args[] ){
+  public static void main(String args[]){
+	  System.out.println("Please select an account:");
+	  Scanner sc = new Scanner(System.in);
+	  
+	  System.out.println("CHEQUE or SAVINGS: ");
+	  
     //This is just a starting point to store the values of each bank account
     // Declaration & instantiation
     
@@ -10,6 +17,6 @@ public class bankaccounts{
     cheque[0] = 100;
     savings[0] = 1000;
     
-    		
+    
   }
 }
