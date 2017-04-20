4ce624cfb1473a4d4c4aa61a6406908990793a3e
mikeman2020 <1996michaelt@gmail.com>
2017-04-20 17:40:44 +1200

    changing arrays
    changed the arrays to variables

--- bankaccounts.java
+++ bankaccounts.java
@@ -1,14 +1,11 @@
 public class bankaccounts{
   public static void main(String args[] ){
     //This is just a starting point to store the values of each bank account
-    // Declaration & instantiation
     
-    int cheque[] = new int [1];
-    int savings[] = new int [1];
     
-    // intitialisation
-    cheque[0] = 100;
-    savings[0] = 1000;
+    int cheque = 0;
+    int savings = 0;
+    
     
     		
   }
